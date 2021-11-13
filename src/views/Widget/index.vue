b0fd9697fa5008660232a95550e287d6f29e0f14
Claudemir Serigheli Junior <cdsjrms@gmail.com>
2021-11-13 17:28:38 -0300

    first commit

---
+++ src/views/Widget/index.vue
@@ -0,0 +1,50 @@
+<template lang="ts">
+  <teleport to="body">
+    <component
+      :is="state.component"
+      @open-box="handleOpenBox"
+      @close-box="handleCloseBox"
+    />
+  </teleport>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
+import Standby from './Standby.vue'
+import Box from './Box.vue'
+
+type State = {
+  component: string
+}
+
+interface SetupReturn {
+  state: State,
+  handleOpenBox(): void;
+  handleCloseBox(): void;
+}
+
+export default defineComponent({
+  components: {
+    Standby,
+    Box
+  },
+  setup (): SetupReturn {
+    const state = reactive<State>({
+      component: 'Standby'
+    })
+
+    function handleOpenBox (): void {
+      state.component = 'Box'
+    }
+
+    function handleCloseBox (): void {
+      state.component = 'Standby'
+    }
+
+    return {
+      state,
+      handleOpenBox,
+      handleCloseBox
+    }
+  }
+})
+</script>
